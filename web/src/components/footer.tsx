77f0a101d8fd9e674ef27d54699f2022de249296
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-23 09:29:39 +0200

    finished website layout

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import gtrade_logo from "../assets/gtrade-logo.png";
+import { BsGithub } from "react-icons/bs";
+import { TbLetterM } from "react-icons/tb";
+import {BsTwitter} from "react-icons/bs";
+import {SiMinutemailer, SiDiscord} from "react-icons/si";
 
 const Footer = () => {
     return (
-        <div className="w-[100%] flex flex-row gap-6">
+        <div className="w-[100%] flex flex-col gap-6 mt-32">
+            <div className="flex justify-center items-center">
+                <img src={gtrade_logo} alt="Gtrade Logo" className="hover:cursor-pointer"/>
+            </div>
+            <div className="flex justify-center items-center flex-row gap-12">
+                <BsTwitter fontSize={"2rem"} color="gray" className=" hover:text-white hover:cursor-pointer"/>
+                <BsGithub fontSize={"2rem"} color="gray" className=" hover:text-white hover:cursor-pointer"/>
+                <TbLetterM fontSize={"2rem"} color="gray" className=" hover:text-white hover:cursor-pointer"/>
+                <SiMinutemailer fontSize={"2rem"} color="gray" className=" hover:text-white hover:cursor-pointer"/>
+                <SiDiscord  fontSize={"2rem"} color="gray" className=" hover:text-white hover:cursor-pointer"/>
+            </div>
+            <div className="mb-4">
+                <p className="text-lg font-thin font-sans text-gray-400 text-center">© 2023 Gtrade. All rights reserved.</p>
+            </div>
         </div>
     )
 }
--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -7,7 +7,7 @@ import gtrade_logo from "../assets/gtrade-logo.png";
 
 const Header = () => {
     return (
-        <div className="w-[100%] h-[8.1vh] flex flex-row gap-6 relative">
+        <div className="w-[100%] h-[8.1vh] flex flex-row gap-6">
             <img src={gtrade_logo} alt="Gtrade Logo ." />
             <div className="flex flex-row justify-start items-center gap-8">
                 <p className="text-[rgb(138,138,150)] text-md font-sans font-semibold hover:cursor-pointer">Trade</p>
@@ -16,7 +16,7 @@ const Header = () => {
                 <p className="text-[rgb(138,138,150)] text-md font-sans font-semibold hover:cursor-pointer">Statistics</p>
                 <p className="text-[rgb(138,138,150)] text-md font-sans font-semibold hover:cursor-pointer">Documentation</p>
             </div>
-            <div className=' w-[60%] h-[100%] relative flex flex-row justify-end items-center'>
+            <div className=' w-[60%] h-[100%] flex flex-row justify-end items-center'>
                 <div className='w-[90%] h-[100%] mr-[5%] gap-8 flex flex-row items-center justify-end'>
                     <button className='h-[5vh] w-[20%] flex flex-row justify-center items-center gap-3 border-[1px] border-solid border-zinc-500 rounded text-[rgb(138,138,150)] text-md font-sans font-semibold'>Connect Wallet <GrPowerShutdown color='rgb(113, 113, 122)' fontSize={'1.3rem'} /></button>
                     <IoSettingsOutline color='rgb(113, 113, 122)' fontSize={'1.5rem'} />
